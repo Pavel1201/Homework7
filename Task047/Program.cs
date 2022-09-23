32e52019e7380fd1b451d0b8102db4d4b5fabcdc
Pavel <p.a.dudarev@mail.ru>
2022-09-23 20:19:44 +0500

    Tasks 47 and 50

---
+++ Task047/Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+int height = EnterSizeofArray("Enter height");
+int width = EnterSizeofArray("Enter width");
+
+double[,] array = Fill2dArray(height,width);
+PrintArray(array,height,width);
+
+
+int EnterSizeofArray(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+
+}
+
+double[,] Fill2dArray(int rows, int columns)
+{
+    double[,] array = new double[rows,columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+        }
+
+
+    }
+    return array;
+}
+
+void PrintArray(double[,] array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{Math.Round(array[i,j],1),5} ");
+    }
+    Console.WriteLine();
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task050/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+int height = EnterSizeofArray("Введите количество строк в массиве");
+int width = EnterSizeofArray("Введите количество столбцов в массиве");
+int[,] numbers =Fill2dArray(height,width);
+FindElement(numbers,height,width);
+PrintArray(numbers,height,width);
+
+void FindElement(int[,] array, int rows, int columns)
+{
+    Console.WriteLine("Введите позицию элемента в двумерном массиве");
+    int row = int.Parse(Console.ReadLine()!);
+    int column = int.Parse(Console.ReadLine()!);
+
+    if (row < rows && column < columns)
+    {
+        Console.WriteLine($"элемент найден , это {array[row - 1,column - 1]} на позиции {(row,column)}");// отчет у пользователя от 1
+    }
+    else
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+
+
+int EnterSizeofArray(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+
+}
+
+int[,] Fill2dArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+        }
+
+
+    }
+    return array;
+}
+
+void PrintArray(int[,] array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{array[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
